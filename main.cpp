e6298d8d6d78faaa3443fdcdadbd8af2f51302b6
niels <185930724+Niels-Hertoghs@users.noreply.github.com>
2025-10-30 15:00:45 +0100

    circle ci test 1000

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1500, 1500), "packman");
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "packman");
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
